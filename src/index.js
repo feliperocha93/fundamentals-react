faa4f7dbb89e8f13e87d900f4f174d86b9a5c632
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-18 19:45:39 -0300

    first component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('root')
+  <App></App>,
+  document.getElementById('root'),
 );
