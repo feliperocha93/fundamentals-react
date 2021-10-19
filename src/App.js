b8fb0fc408f5bda9741e5cade864f462d0b0694c
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-19 11:34:00 -0300

    Learning about props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
 
 import Post from "./Post";
+import Header from "./Header";
 
 function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <Header title="JStack's Blog">
+        <h2>Posts da Semana</h2>
+      </Header>
 
       <hr />
 
       <Post 
+        likes={11}
         post={{
           title: 'Oi né',
           subtitle: 'subtítulo da noticia 01'
         }}
       />
       <Post 
+        likes={5}
         post={{
           title: 'Oi né',
           subtitle: 'subtítulo da noticia 02'
         }}
       />
       <Post 
+        likes={18}
         post={{
           title: 'Oi né',
           subtitle: 'subtítulo da noticia 03'
