b80161192d9f2de211216a7c67cdd1ca23e2828e
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-20 23:43:54 -0300

    Working with themes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
-export default function Layout() {
+export default function Layout({
+  onToggleTheme, selectedTheme
+}) {
+  const theme = useTheme();
+
   return (
     <>
-      <Header />
+      <Header
+        onToggleTheme={onToggleTheme}
+        selectedTheme={selectedTheme}
+      />
       <PostsList />
-      <Footer />
+      <Footer
+        onToggleTheme={onToggleTheme}
+        selectedTheme={selectedTheme}
+      />
+      <div
+        style={{
+          marginTop: 24,
+          backgroundColor: theme.footerBackgroundColor,
+          padding: 24,
+        }}
+      >
+        Oi, né?
+      </div>
     </>
   );
 }
